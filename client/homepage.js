8f59ee1c09fd96d267e74813d5cb277eb6e5bad6
Alexandrakis Alexandros <aalexandrakis@hotmail.com>
2015-01-24 00:30:04 +0200

    -- some changes and popups

--- client/homepage.js
+++ client/homepage.js
@@ -1,4 +1,10 @@
-var phone = "+306948211181";
+var phone = "+15516893490";
+
+var modalTexts = {
+    locations : "Secaucus, Rutherford, Newark, Manhattan, Bronx",
+    services : "<p>Xpress taxi and limo services offers luxury transportation service to all Airports , Atlantic City Casinos, Jersey Gardens , Woodbury Commons  and more destinations.</p><p>Travel from your home or office to the airport and arrive rested and without the worry and expense of airport parking or car rentals.</p>",
+}
+
 Template.homepage.helpers({
     "phone" : function(){
         return phone;
@@ -6,11 +12,15 @@ Template.homepage.helpers({
 });
 
 Template.homepage.events({
-    "mouseover #callNumber" : function(e, t){
-        t.find("#callNumber").className="timeline-badge success";
+    "click #locationsBtn" : function(e, t){
+        t.find("#modalTitle").innerHTML="Locations";
+        t.find("#modalText").innerHTML=modalTexts.locations;
+        $("#modalDialog").modal('show');
     },
-    "mouseout #callNumber" : function(e, t){
-        t.find("#callNumber").className="timeline-badge primary";
+    "click #servicesBtn" : function(e, t){
+        t.find("#modalTitle").innerHTML="Services";
+        t.find("#modalText").innerHTML=modalTexts.services;
+        $("#modalDialog").modal('show');
     }
 });
 
---
+++ client/publicMethods.js
@@ -0,0 +1,9 @@
+//existsInArray = function (array, checkFor, callback){
+//        array.forEach(function(str){
+//            if (str == checkFor){
+//                callback();
+//                return;
+//            }
+//        });
+//        return false;
+//}
